3a8fcf16e6d795b302da80fcab21514fa71339d7
Chandan Kumar <chandankumar26102000@gmail.com>
2025-07-06 20:28:30 +0530

    push all python file

---
+++ 5-import/package/__init__.py
@@ -0,0 +1,3 @@
+"""
+    __init__.py is a special file 
+"""
---
+++ 5-import/test.py
@@ -0,0 +1,6 @@
+from package.math import *
+from package.subpackage.mul import multiply
+
+print(addition(2,6))
+print(substraction(4,8))
+print(multiply(5,10))
